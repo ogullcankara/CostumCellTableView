f7e26a536f639beec16b8ba8a67157f8cdabe9ba
ogullcankara <ogullcankara@gmail.com>
2022-08-29 17:44:06 +0300

    Initial Commit

---
+++ CostumCellTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CostumCellTableView
+//
+//  Created by Ogulcan Kara on 29.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
